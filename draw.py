9a9d4f9ee0f5ff48ec295c3c4626443b3307b609
sachin801 <sachinsahara2002@gmail.com>
2021-10-06 20:45:24 +0530

    adding text and drawing line on image

--- draw.py
+++ draw.py
@@ -15,14 +15,23 @@ blank=np.zeros((500,500,3),dtype='uint8')
 cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
 cv.imshow('Rectangle', blank)
 
-cv.waitKey(0)
+#cv.waitKey(0)
 
 #creating a circle
-cv.circle(blank,(blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=-1)
-cv.imshow('circle',blank)
+cv.circle(blank, (blank.shape[1]//2,blank.shape[0]//2), 40, (0,0,255), thickness=-1)
+cv.imshow('circle', blank)
 #
 # img = cv.imread('Photos/cat.jpg')
 # cv.imshow('Cat',img)
 #
 #
+#cv.waitKey(0)
+
+#creating a line
+cv.line(blank, (100,50), (blank.shape[1]//2,blank.shape[0]//2), (255,255,255),thickness=3)
+cv.imshow('Line', blank)
+
+#put text on image
+cv.putText(blank, 'Hello I am sachin', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255,255,0), thickness=3)
+cv.imshow('TExt', blank)
 cv.waitKey(0)
